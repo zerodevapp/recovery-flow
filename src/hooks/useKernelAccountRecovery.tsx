d3eacf1e750c022293e29b7987bdd89af55992b1
Blake Duncan <blakecduncan@gmail.com>
2024-01-12 14:56:13 -0500

    Add isPending prop

--- src/hooks/useKernelAccountRecovery.tsx
+++ src/hooks/useKernelAccountRecovery.tsx
@@ -29,11 +29,19 @@ type UseKernelAccountRecoveryResult = {
    * List of guardian addresses
    */
   guardians: string[];
+
+  /**
+   * Returns true if the process to set a guardian has been initiated but not yet confirmed
+   */
+  isPending: boolean;
 };
 
 const useKernelAccountRecovery = ({ address, onSetupGuardianRequest, chainId }: RecoveryConfig): UseKernelAccountRecoveryResult => {
   const childWindowRef = useRef<Window | null>(null);
   const [error, setError] = useState<string | undefined>(undefined);
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const [popupCheckInterval, setPopupCheckInterval] = useState<number | null>(null);
+  const [requestType, setRequestType] = useState<'enableRecovery' | 'deleteRecovery' | undefined>(undefined);
 
   const { data } = useSWR(
     address ? `${KERNEL_API_URL}/accounts/${address}/guardians` : null, 
@@ -46,13 +54,29 @@ const useKernelAccountRecovery = ({ address, onSetupGuardianRequest, chainId }: 
       return [];
     }
 
+    if (data.length > 0 && requestType === 'enableRecovery') {
+      setIsPending(false);
+      setRequestType(undefined);
+    }
+    if (data.length === 0 && requestType === 'deleteRecovery') {
+      setIsPending(false);
+      setRequestType(undefined);
+    }
+
     return data.map((guardian: any) => guardian.guardian);
   }, [data]);
+
+  const checkPopupWindow = useCallback(() => {
+    if (childWindowRef.current && childWindowRef.current.closed) {
+      setIsPending(false);
+    }
+  }, []);
 
   const openRecoveryPopup = useCallback(() => {
     if (address === undefined || chainId === undefined) {
       return;
     }
+    setIsPending(true);
     const parentUrl = encodeURIComponent(window.location.origin);
     const dashboardUrl = `${RECOVERY_DASHBOARD_URL}/recovery-setup/${address}?parentUrl=${parentUrl}&chainId=${chainId}`;
     const windowFeatures = 'width=400,height=720,resizable,scrollbars=yes,status=1';
@@ -60,8 +84,19 @@ const useKernelAccountRecovery = ({ address, onSetupGuardianRequest, chainId }: 
 
     if (childWindowRef.current) {
       childWindowRef.current.focus();
+      if (!popupCheckInterval) {
+        const interval = setInterval(checkPopupWindow, 1000); // Check every second
+        setPopupCheckInterval(interval);
+      }
     }
-  }, [address]);
+  }, [address, checkPopupWindow, popupCheckInterval, chainId]);
+
+  useEffect(() => {
+    if (popupCheckInterval && !isPending) {
+      clearInterval(popupCheckInterval);
+      setPopupCheckInterval(null);
+    }
+  }, [popupCheckInterval, isPending]);
 
   useEffect(() => {
     const handleMessage = async (event: MessageEvent) => {
@@ -70,10 +105,12 @@ const useKernelAccountRecovery = ({ address, onSetupGuardianRequest, chainId }: 
         return;
       }
       if (error) {
+        // Reset the error if there was one
         setError(undefined);
       }
 
-      const { userOp } = event.data;
+      const { userOp, request } = event.data;
+      setRequestType(request);
       const parseUserOpCallData = validateUserOperationCallData(userOp);
 
       if (!parseUserOpCallData.success) {
@@ -100,14 +137,18 @@ const useKernelAccountRecovery = ({ address, onSetupGuardianRequest, chainId }: 
 
     return () => {
       window.removeEventListener('message', handleMessage);
+      if (popupCheckInterval) {
+        clearInterval(popupCheckInterval);
+      }
     };
-  }, [onSetupGuardianRequest]);
+  }, [onSetupGuardianRequest, popupCheckInterval]);
 
   return { 
     openRecoveryPopup,
     error,
     recoveryEnabled: guardians.length > 0,
     guardians,
+    isPending
   };
 };
 
